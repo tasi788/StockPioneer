5c7508880ff8475550a988ea855ec912ccc20e5f
tdc <tdc@tdccc.com.tw>
2020-02-05 10:52:19 +0800

    add comment

--- StockPioneer/command/ping.py
+++ StockPioneer/command/ping.py
@@ -1,9 +1,10 @@
 from telegram.ext import run_async, CallbackContext
 from telegram.update import Update
 
+
 @run_async
-def ping(update:Update, context: CallbackContext):
-    """
+def ping(update: Update, context: CallbackContext):
+    """ping
     Ping Pong
     """
     update.message.reply_text('pong')
